8657ed6df0a6f66827aff5af37f3acae110ab28c
scottsalvaggio <scottsalvaggio@gmail.com>
2018-07-08 01:39:13 +0000

    Added remaining files (including those from distribution code). Updated import.py (working version with import confirmations).

--- import.py
+++ import.py
@@ -14,20 +14,25 @@ def main():
     f = open("zips.csv")
     reader = csv.reader(f)
 
-    # Skip the first row (which contains column headings)
+    # Print and skip the first row (which contains column headings).
+    line_num = 1
+    print(f"Skipped line {line_num} (column headings)")
     next(reader, None)
+    line_num += 1
 
     # Iterate over the rows of the opened CSV file.
     for row in reader:
 
         # Execute database queries, one per row; then print out confirmation.
-        # Convert zip_code (row[0]) to a 5-character string (with leading zeros if needed)
+        # Convert zip_code (row[0]) to a 5-character string (with leading zeros if needed).
         db.execute("INSERT INTO locations (zip_code, city, state, latitude, longitude, population) \
                     VALUES (:zip_code, :city, :state, :latitude, :longitude, :population)",
                    {"zip_code": str(row[0]).zfill(5), "city": row[1], "state": row[2], "latitude": row[3],
                     "longitude": row[4], "population": row[5]})
+        print(f"Added line {line_num}: {row[0]},{row[1]},{row[2]},{row[3]},{row[4]},{row[5]}")
+        line_num += 1
 
-    # Technically this is when all of the queries we've made happen!
+    # Commit to database.
     db.commit()
 
 

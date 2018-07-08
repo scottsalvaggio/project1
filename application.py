26956084213bbe2d094f954e6d26e0ade4795def
scottsalvaggio <scottsalvaggio@gmail.com>
2018-07-08 22:24:20 +0000

    Added template files with Register and Login form fields.

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, render_template, request, session
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
@@ -17,10 +17,17 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 # Set up database
-engine = create_engine(os.getenv("DATABASE_URL"))
+engine = create_engine(os.getenv("postgres://pezufjjvmkmciq:c0798f396cc8f2813bbbd08c28c9833260c3274ba4ff15670c93bac5b3777c1c@ec2-23-23-93-115.compute-1.amazonaws.com:5432/d2ujn0f3h1cj9j"))
 db = scoped_session(sessionmaker(bind=engine))
-
 
 @app.route("/")
 def index():
-    return "Project 1: TODO"
+    return render_template("index.html")
+
+@app.route("/register", methods=["POST"])
+def register():
+    return render_template("success.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    return render_template("results.html", message="You are logged in and results are coming soon!")

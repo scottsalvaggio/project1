4da88ebf8ddfd7ddcb26ff7b6bf82c0992ae0dc5
scottsalvaggio <scottsalvaggio@gmail.com>
2018-07-12 08:10:24 +0000

    Added commands.sql

---
+++ commands.sql
@@ -0,0 +1,25 @@
+-- CREATE TABLE commands
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username VARCHAR UNIQUE NOT NULL,
+    password VARCHAR NOT NULL
+);
+
+CREATE TABLE locations (
+    id SERIAL PRIMARY KEY,
+    zip_code VARCHAR UNIQUE NOT NULL,
+    city VARCHAR NOT NULL,
+    state VARCHAR NOT NULL,
+    latitude DECIMAL,
+    longitude DECIMAL,
+    population INTEGER
+);
+
+CREATE TABLE check_ins (
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER REFERENCES users,
+    location_id INTEGER REFERENCES locations,
+    comment VARCHAR,
+    UNIQUE (user_id, location_id)
+);
